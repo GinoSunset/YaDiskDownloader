27068b2080a252d2f38176e95d9e7d9930fd01fe
6ejibiu <akimovpnz@live.com>
2020-08-22 10:43:11 +0300

    add download folder recursive

--- backup.py
+++ backup.py
@@ -6,6 +6,21 @@ import pathlib
 import yadisk
 
 
+def download_folder(folder, path_to_save):
+    logging.error(f"start download folder {folder}")
+    if not (y.exists(folder)):
+        logging.error(f"Folder {folder} not exists")
+        return
+    pathlib.Path(f"{date}/{folder}").mkdir(parents=True, exist_ok=True)
+    for file_ in y.listdir(folder):
+        if file_.type == "dir":
+            download_folder(f"{folder}/{file_.name}", f"{path_to_save}/{file_.name}")
+        if file_.type == "file":
+            logging.error(f"    start download file {file_.path}")
+            y.download(file_.path, f"{date}/{folder}/{file_.name}")
+    logging.error(f"Success download folder {folder}")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -24,12 +39,5 @@ if __name__ == "__main__":
     y = yadisk.YaDisk(token=args.token)
     date = datetime.strftime(datetime.now(), "%d.%m.%Y-%H.%M.%S")
     for folder in args.dirs:
-        pathlib.Path(f"{date}/{folder}").mkdir(parents=True, exist_ok=True)
-        if not (y.exists(folder)):
-            logging.error(f"Folder {folder} not exists")
-            continue
-        for file_ in y.listdir(folder):
-            # TODO: look directory
-            # TODO: make repository
-            y.download(folder, f"{date}/{folder}")
+        download_folder(folder, f"{date}/{folder}")
 
